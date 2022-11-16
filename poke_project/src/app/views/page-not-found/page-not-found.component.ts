4ba5f0305708ef13854538be65ff8bc75d6e8ec6
RogeMB <mohigefer.barog22@triana.salesianos.edu>
2022-11-16 13:17:54 +0100

    minor bugs fixed

--- poke_project/src/app/views/page-not-found/page-not-found.component.ts
+++ poke_project/src/app/views/page-not-found/page-not-found.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { History } from 'swiper';
 
 @Component({
   selector: 'app-page-not-found',
